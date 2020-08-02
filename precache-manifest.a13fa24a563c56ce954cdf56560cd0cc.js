8b01e9101dff67936b8ecafa1d2fa04d6d9ce69e
Joona Brückner <zockedidockhd@gmail.com>
2020-08-02 13:30:19 +0200

    Updates

---
+++ precache-manifest.a13fa24a563c56ce954cdf56560cd0cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82cfbbae94474c23f1e8c16ae82ed62b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "865299072454fd012375",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "750894e0919366031b12",
+    "url": "/static/js/2.615c6fc2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.615c6fc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "865299072454fd012375",
+    "url": "/static/js/main.0b0716d9.chunk.js"
+  },
+  {
+    "revision": "241f87120d285ee26494",
+    "url": "/static/js/runtime-main.56ae10a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
